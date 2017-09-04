d68327cff4d156e9354dd3d866bb1be7346da561
Kazuki Oishi <oishikazuki@gmail.com>
2017-09-05 06:50:30 +0900

    foobar2000プラグインにカバーアート送信機能を追加

--- foo_nowplayingv2/foo_nowplayingv2/main.cpp
+++ foo_nowplayingv2/foo_nowplayingv2/main.cpp
@@ -17,11 +17,31 @@ void play_callback_minato_yukina::on_playback_new_track(metadb_handle_ptr p_trac
     //get playing file info
     file_info_impl fileinfo;
     p_track->get_info(fileinfo);
+    abort_callback_impl abortcallback;
+    pfc::list_single_ref_t<metadb_handle_ptr> tracklist(p_track);
+    pfc::list_single_ref_t<GUID> guidlist(album_art_ids::cover_front);
+    auto iptr = static_api_ptr_t<album_art_manager_v3>()->open_v3(tracklist, guidlist, NULL, abortcallback);
+    QByteArray qcoverary;
+    try {
+        auto dataptr = iptr->query(album_art_ids::cover_front, abortcallback);
+        auto memptr = dataptr->get_ptr();
+        std::stringstream ss;
+        ss << memptr;
+        console::print((std::string("cover address:") + ss.str()).c_str());
+        console::print((std::string("cover size:") + std::to_string(dataptr->get_size())).c_str());
+        std::unique_ptr<char[]> coverdataptr(new char[dataptr->get_size()]);
+        memcpy(coverdataptr.get(), dataptr->get_ptr(), dataptr->get_size());
+        qcoverary = QByteArray::QByteArray(coverdataptr.get(), dataptr->get_size());
+    }
+    catch (exception_album_art_not_found) {
+        console::print("albumart not found");
+    }
     QJsonObject jsonobj{
         {"title",fileinfo.meta_exists("TITLE") ? QString(fileinfo.meta_get("TITLE",0)) : "" },
         {"album",fileinfo.meta_exists("ALBUM") ? QString(fileinfo.meta_get("ALBUM",0)) : "" },
         {"artist",fileinfo.meta_exists("ARTIST") ? QString(fileinfo.meta_get("ARTIST",0)) : "" },
-        {"albumartist",fileinfo.meta_exists("ALBUM ARTIST") ? QString(fileinfo.meta_get("ALBUM ARTIST",0)) : "" }
+        {"albumartist",fileinfo.meta_exists("ALBUM ARTIST") ? QString(fileinfo.meta_get("ALBUM ARTIST",0)) : "" },
+        {"albumart",QString(qcoverary.toBase64())}
     };
     QJsonDocument jsondoc(jsonobj);
     QByteArray sendary = jsondoc.toJson();
--- foo_nowplayingv2/foo_nowplayingv2/stdafx.h
+++ foo_nowplayingv2/foo_nowplayingv2/stdafx.h
@@ -1,4 +1,7 @@
 #pragma once
 #include "../SDK/foobar2000/SDK/foobar2000.h"
+#include "../SDK/pfc/list.h"
 #include <memory>
 #include <thread>
+#include <sstream>
+#include <string>
