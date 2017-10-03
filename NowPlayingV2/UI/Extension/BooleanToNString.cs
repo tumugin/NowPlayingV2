e570922e9cc8190946b2d78656a59697752d7770
Kazuki Oishi <oishikazuki@gmail.com>
2017-10-03 21:43:32 +0900

    UI周りをいろいろ実装

---
+++ NowPlayingV2/UI/Extension/BooleanToNString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NowPlayingV2.UI.Extension
+{
+    public class BooleanToNString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool?)value == true) ? "有効" : "無効";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NowPlayingV2/UI/OAuthWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NowPlayingV2.UI
+{
+    /// <summary>
+    /// OAuthWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class OAuthWindow : MetroWindow
+    {
+        public OAuthWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
