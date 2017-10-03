22d83583126dc3485838174bc5d73774448ff1a2
Kazuki Oishi <oishikazuki@gmail.com>
2017-10-04 02:23:19 +0900

    認証画面のUIを少し実装

---
+++ NowPlayingV2/Tsumugi/AccountManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowPlayingV2.Tsumugi
+{
+    public class AccountManager
+    {
+        public AccountManager()
+        {
+
+        }
+
+        public void OpenAuthDialog()
+        {
+            
+        }
+
+        public string GetAuthURL()
+        {
+            return null;
+        }
+
+        public void AuthWithPin(string pin)
+        {
+
+        }
+    }
+}
